c65569dd7eb214d4acfac9f1a400ad1b578496eb
Martyn Gigg <martyn.gigg@gmail.com>
2016-04-06 09:39:22 +0100

    Add helper scripts for cowbuilder environments

---
+++ README.md
@@ -0,0 +1,8 @@
+Cowbuilder
+----------
+
+Some rough and ready helper scripts for:
+
+* setting up a cowbuilder environment
+* build a binary package
+* building a source package
---
+++ pbuilder-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Takes a the name of the distribution and builds a package
+#
+
+if [ ! $# -eq 1 ]; then
+    echo "Usage: $0 codename"
+    exit 1
+fi
+
+if [ ! -f $HOME/.pbuilderrc ]; then
+  echo "$HOME/.pbuilderrc does not exist. Please run this script with sudo HOME=\$HOME $0"
+  exit 1
+fi
+
+DISTRIB=$1
+COW_BASE=/var/cache/pbuilder/$DISTRIB-amd64/base.cow
+
+DIST=$DISTRIB pdebuild --use-pdebuild-internal --pbuilder \
+    cowbuilder -- --basepath=$COW_BASE
